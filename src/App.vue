a95084027a3682a88e6361b62a43438f8e36ff02
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-21 15:10:57 +0200

    add appTitle

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,23 @@
 <script>
+import appTitle from './components/AppTitle.vue'
+
  export default{
     data() {
       return {
         message: "Hello Vite"
       }
-    }
+      
+    },
+    components: {
+        appTitle
+    },
   }
 </script>
 
 <template>
   <h1>{{message}}</h1>
+  <appTitle />
+  
 </template>
 
 <style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,8 @@
+<script>
+
+</script>
+
+<template>
+    <h2>La mia prima app con vite!</h2>
+    
+</template>
